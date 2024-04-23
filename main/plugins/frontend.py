c56996da2db5ba2e537242ab117c832ad3a4f659
speeed <166220396+speed-666@users.noreply.github.com>
2024-04-23 13:48:58 +0200

    Update frontend.py

--- main/plugins/frontend.py
+++ main/plugins/frontend.py
@@ -2,7 +2,7 @@ import time, os
 
 from .. import bot as Drone
 from .. import userbot, Bot
-from .. import FORCESUB as اشتراك_قسري
+from .. import FORCESUB as fs
 from main.plugins.pyroplug import get_msg
 from main.plugins.helpers import get_link, join
 
@@ -11,15 +11,15 @@ from pyrogram.errors import FloodWait
 
 from ethon.telefunc import force_sub
 
-اشتراك_قسري_رسالة = f"**✨ يجب عليك الانضمام إلى @{اشتراك_قسري} لاستخدام هذا البوت. ✨**"
+ft = f"**✨ يجب عليك الانضمام إلى @{fs} لاستخدام هذا البوت. ✨**"
 
-رسالة_بداية = "**📩 أرسل لي رابط الرسالة التي تريد بدء الحفظ منها كرد على هذه الرسالة. 📩**"
+message = "**📩 أرسل لي رابط الرسالة التي تريد بدء الحفظ منها كرد على هذه الرسالة. 📩**"
 
 @Drone.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def clone(event):
     if event.is_reply:
         reply = await event.get_reply_message()
-        if reply.text == رسالة_بداية:
+        if reply.text == message:
             return
     try:
         link = get_link(event.text)
@@ -27,7 +27,7 @@ async def clone(event):
             return
     except TypeError:
         return
-    s, r = await force_sub(event.client, اشتراك_قسري_رسالة, event.sender_id, اشتراك_قسري_رسالة)
+    s, r = await force_sub(event.client, fs, event.sender_id, ft)
     if s == True:
         await event.reply(r)
         return
@@ -40,7 +40,7 @@ async def clone(event):
         if 't.me/' in link:
             await get_msg(userbot, Bot, Drone, event.sender_id, edit.id, link, 0)
     except FloodWait as fw:
-        return await event.reply(f"**⏳ حاول مرة أخرى بعد {fw.x} ثانية بسبب انتظار التحكم في الفيض من تليجرام. ⏳**")
+        return await tcrep1.send_message(event.sender_id, f"**⏳ حاول مرة أخرى بعد {fw.x} ثانية بسبب انتظار التحكم في الفيض من تليجرام. ⏳**")
     except Exception as e:
         print(e)
-        await event.reply(f"**❌ حدث خطأ أثناء عملية الاستنسخ من `{link}`\n\nالخطأ:** {str(e)} ❌**")
+        await tcrep1.send_message(event.sender_id, f"**❌ حدث خطأ أثناء عملية الاستنساخ من `{link}`\n\nالخطأ:** {str(e)} ❌**")
