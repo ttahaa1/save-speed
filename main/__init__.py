0033f77edce68aebe05bd4b539c384d739a80a70
speed <153678863+ttahaa1@users.noreply.github.com>
2024-05-13 18:58:36 +0300

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,4 +1,4 @@
-#Github.com/Vasusen-code
+# Github.com/Vasusen-code
 
 from pyrogram import Client
 
@@ -16,7 +16,6 @@ API_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
 SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
 AUTH = config("AUTH", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
